7aa886661e21e290e666b61ec256ba65c9d9443e
MattDaniel-hub <daniel-ich99@gmx.de>
2022-11-05 14:21:49 +0100

    setup sample project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java / Maven sample project
+
+Structure created with:
+```
+mvn archetype:generate -DgroupId=com.mySampleApp101.app -DartifactId=my-sample-app-101 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+```
---
+++ src/main/java/com/mySampleApp101/app/Main.java
@@ -0,0 +1,15 @@
+package com.mySampleApp101.app;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+
+        JOptionPane.showInputDialog(null, args);
+    }
+}
